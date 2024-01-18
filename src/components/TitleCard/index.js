f05d45296cec4e43fb74bf0d11d7628dcc40ed31
HK Transfield <h.k.transfield1@gmail.com>
2024-01-18 13:33:34 +1300

    feat: add fade in animation to titlecard

--- src/components/TitleCard/index.js
+++ src/components/TitleCard/index.js
@@ -1,9 +1,9 @@
 'use client';
+import './index.css';
 import React from 'react';
-import { ThemeProvider, Typography, createTheme, Box, CssBaseline } from '@mui/material';
-import localFont from "@next/font/local";
+import { ThemeProvider, Typography, CssBaseline } from '@mui/material';
 import theme from '@/theme';
-
+import { Animate, AnimateGroup, useAnimate }  from 'react-simple-animate';
 
 /**
  * Display a header text that spans the entire screen
@@ -12,10 +12,25 @@ import theme from '@/theme';
  * @returns
  */
 export default function TitleCard(props) {
+
+    // hook for simple transition animation
+    const { play, style } = useAnimate({
+        start: { opacity: 0 },
+        end: { opacity: 1 }
+      });
+
+
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
-            <Typography variant='h1'>{props.titleText}</Typography>
+            <Animate
+                play={true}
+                duration={3}
+                start={{ opacity: 0, filter: 'blur(10px)' }}
+                end={{ opacity: 1, filter: 'blur(0)' }}
+            >
+                <Typography className='shine' variant='h1'>{props.titleText}</Typography>
+            </Animate>
         </ThemeProvider>
     );
 }
