16b2ba289f1bfec9916593590c5ab0443668dc54
HK Transfield <h.k.transfield1@gmail.com>
2024-02-13 14:50:26 +1300

    feat: integrate frontendmentor signup component

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,16 @@ import Image from 'next/image';
 import Landing from '@/layouts/Landing';
 import TitleCard from '@/components/TitleCard';
 import SignInSide from '@/components/SignInSide';
+import SignUp from '@/components/SignUp/SignUp';
 
 export default function Home() {
   const titleText = "Dungeon As Bro"
 
   return (
     <Landing>
-      <TitleCard titleText={titleText}/>
-      <SignInSide/>
+      {/* <TitleCard titleText={titleText}/>
+      <SignInSide/> */}
+      <SignUp/>
     </Landing>
   )
 };
--- src/components/SignInSide/index.js
+++ src/components/SignInSide/index.js
@@ -1,5 +1,5 @@
 'use client';
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -7,13 +7,71 @@ import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
+import { Grid, IconButton } from '@mui/material';
+import {
+  Facebook,
+  Twitter,
+  Instagram,
+  LinkedIn,
+  YouTube,
+} from '@mui/icons-material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import SendIcon from '@mui/icons-material/Send';
+import { styled } from '@mui/material/styles';
 import emailjs from '@emailjs/browser';
+import autoAnimate from '@formkit/auto-animate';
 
+/**
+ * Functional component that displays a grid of social media icons.
+ * @returns
+ */
+function SocialMediaGrid() {
+
+   /**
+   * TODO: Move form off to the right more.
+   * TODO: Change opacity of the form background
+   * TODO: Add blur to the form background
+   */
+  return (
+    <Grid container spacing={2}>
+      <Grid item>
+        <IconButton aria-label="Facebook" href="https://www.facebook.com/profile.php?id=100092327695529">
+          <Facebook />
+        </IconButton>
+      </Grid>
+      <Grid item>
+        <IconButton aria-label="Twitter">
+          <Twitter />
+        </IconButton>
+      </Grid>
+      <Grid item>
+        <IconButton aria-label="Instagram" href="https://www.instagram.com/dungeonas_bro/">
+          <Instagram />
+        </IconButton>
+      </Grid>
+      <Grid item>
+        <IconButton aria-label="LinkedIn" href="https://www.linkedin.com/company/dungeon-as-bro/about/">
+          <LinkedIn />
+        </IconButton>
+      </Grid>
+      <Grid item>
+        <IconButton aria-label="YouTube">
+          <YouTube />
+        </IconButton>
+      </Grid>
+    </Grid>
+  );
+}
+
+/**
+ *
+ * @param {*} props
+ * @returns
+ */
 function Copyright(props) {
+
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
@@ -32,14 +90,6 @@ const defaultTheme = createTheme();
 
 export default function SignInSide() {
 
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm();
-  }
-
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -52,14 +102,27 @@ export default function SignInSide() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Grid container component="main">
+
         <CssBaseline />
         <Grid
           item
           xs={false}
           sm={4}
           md={7}
+
         />
-        <Grid item xs={7} sm={8} md={5} component={Paper} elevation={6} alignItems="center" square>
+        <Grid
+          item
+          square
+          xs={7}
+          sm={8}
+          md={4}
+          component={Paper}
+          elevation={6}
+          sx={{
+            backgroundColor: '#005F73'
+          }}
+          >
           <Box
             sx={{
               my: 4,
@@ -82,29 +145,43 @@ export default function SignInSide() {
                 margin="normal"
                 required
                 fullWidth
+                id="name"
+                label="Enter your name"
+                name="name"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
                 id="email"
-                label="Email Address"
+                label="Enter your email"
                 name="email"
                 autoComplete="email"
-                autoFocus
               />
               <Button
                 type="submit"
                 fullWidth
                 variant="contained"
-                sx={{ mt: 3, mb: 2 }}
+                sx={{
+                  color: "#F7FF",
+                  mt: 3,
+                  mb: 2,
+                }}
+                endIcon={<SendIcon />}
               >
                 Sign In
               </Button>
 
+              <SocialMediaGrid sx={{ mx: 'auto' }}/>
+              <Copyright sx={{ mt: 5, mb: 3 }} />
               <Box
+                sx={{ mx: 'auto' }}
                 component={'img'}
                 src="https://placeholder-cover-image.s3.ap-southeast-2.amazonaws.com/Insignia-black.png"
                 height={.25}
                 width={.25}
               />
-
-              <Copyright sx={{ mt: 5 }} />
             </Box>
           </Box>
         </Grid>
